17579538bc8f5a196dd493ee1f8ec7cf46f95add
Amna Ali <amnaali.cheema@gmail.com>
2015-09-08 00:44:10 +0500

    First Commit

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Amna Blog</title>
+    <link href="fonts/play.css" rel="stylesheet"/>
+    <link href="css/reset.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
+</head>
+
+<body>
+    <div class="wrapper">       
+        <header id="mainHeader">
+            <a href="index.php" class="main_logo"><img src="images/logo3.png"></a>
+            <h1 class="slogan">Solutions for a small planet</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#" class="active">About Us</a>
+                <a href="#">Contact Info</a>
+            </nav>
+            
+        </header>
+        <div class="main_content">
+            <p>Put your contents here</p>
+        </div>
+        <div class="right_content">
+            <p>Put your contents here</p>
+        </div>
+    </div>
+    <footer class="main_footer">
+        <p>Copyright © 2015 Amna Ali Blog. All Rights Reserved.</p>
+    </footer>
+</body>
+</html>
